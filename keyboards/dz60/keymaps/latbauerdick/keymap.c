ce7c1638767bcd8d3b15b0dd4353a3d2b13aaf5c
LATBauerdick <github@bauerdick.org>
2025-11-16 11:00:09 -0600

    2025-11-16 making DZ60 Colemak compliant

--- keyboards/dz60/keymaps/latbauerdick/keymap.c
+++ keyboards/dz60/keymaps/latbauerdick/keymap.c
@@ -33,9 +33,9 @@ enum dz60_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT(
           QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0   , KC_MINS, KC_EQL , KC_GRV , KC_BSPC,
-          KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_QUOT, KC_J,    KC_L,    KC_U,    KC_Y,    KC_LBRC, KC_RBRC, KC_BSLS,
-          CTL_ESC, NAV_A,   KC_R,    KC_S,    KC_T,    KC_G,    KC_SCLN, KC_M,    KC_N,    KC_E,    KC_I,    KC_O   ,          KC_ENT ,
- KC_LSFT, LSF_Z  , LSF_Z  , KC_X,    KC_C,    KC_D,    KC_V,    KC_SLSH, KC_K,    KC_H,    KC_COMM, KC_DOT,           RSF_ENT, ADJ_ENT,
+          KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_QUOT, KC_J,    KC_L,    KC_U,    KC_Y,    KC_LBRC, KC_RBRC, KC_BSLS,
+          CTL_ESC, NAV_A,   KC_R,    KC_S,    KC_T,    KC_D,    KC_SCLN, KC_H,    KC_N,    KC_E,    KC_I,    KC_O   ,          KC_ENT ,
+ KC_LSFT, LSF_Z  , LSF_Z  , KC_X,    KC_C,    KC_V,    KC_B,    KC_SLSH, KC_K,    KC_M,    KC_COMM, KC_DOT,           RSF_ENT, ADJ_ENT,
  KC_HYPR, ALT_ESC, GUI_ENT,          GUI_SPC,          RSF_SPC,          RSE_SPC,          RGU_SPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
     [_QWERTY] = LAYOUT(
